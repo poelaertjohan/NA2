df448f0e0219c0b71078b1929ef91413b4c96499
johan poelaert <johan.poelaert@gmail.com>
2018-11-17 15:55:41 +0100

    register and login work
    still have to change the gui

---
+++ Shopping List/Shopping List/Controller/LoginController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginController.swift
+//  Shopping List
+//
+//  Created by johan poelaert on 17/11/2018.
+//  Copyright © 2018 johan poelaert. All rights reserved.
+//
+
+import Foundation
---
+++ Shopping List/Shopping List/Controller/RegisterController.swift
@@ -0,0 +1,9 @@
+//
+//  RegisterController.swift
+//  Shopping List
+//
+//  Created by johan poelaert on 17/11/2018.
+//  Copyright © 2018 johan poelaert. All rights reserved.
+//
+
+import Foundation
