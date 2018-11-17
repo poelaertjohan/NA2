0340d115efe6e84d24f5198dfd59916d00b12e7d
johan poelaert <johan.poelaert@gmail.com>
2018-11-17 11:33:36 +0100

    creating project

---
+++ Shopping List/Shopping List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shopping List
+//
+//  Created by johan poelaert on 17/11/2018.
+//  Copyright © 2018 johan poelaert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
