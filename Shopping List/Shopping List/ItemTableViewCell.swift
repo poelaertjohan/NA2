aed8057ad6679dde3e95bd45a74509240cde8163
johan poelaert <johan.poelaert@gmail.com>
2018-12-28 20:46:12 +0100

    added itemviewlistcell again

--- Shopping List/Shopping List/ItemTableViewCell.swift
+++ Shopping List/Shopping List/ItemTableViewCell.swift
@@ -1,27 +1,9 @@
 //
 //  ItemTableViewCell.swift
-//  BoringSSL
+//  Shopping List
 //
-//  Created by johan poelaert on 19/11/2018.
+//  Created by johan poelaert on 28/12/2018.
+//  Copyright © 2018 johan poelaert. All rights reserved.
 //
 
-import UIKit
-
-class ItemTableViewCell: UITableViewCell {
-
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var pictureImageView: UIImageView!
-    @IBOutlet weak var amountLabel: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
-}
+import Foundation
